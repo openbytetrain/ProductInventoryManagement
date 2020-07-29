97f1515eebf923bc718cf6413c8c4fb9fb72f082
Egle Palk <eglepalk@gmail.com>
2020-07-29 13:25:24 +0300

    PriceAlternationModel mapper fix

--- src/main/kotlin/com/bytetrain/prodinv/service/mapper/PriceAlterationModelMapper.kt
+++ src/main/kotlin/com/bytetrain/prodinv/service/mapper/PriceAlterationModelMapper.kt
@@ -1,10 +1,9 @@
 package com.bytetrain.prodinv.service.mapper
 
-import com.bytetrain.prodinv.domain.Price
 import com.bytetrain.prodinv.service.mapper.factory.PriceAlterationFactory
 import com.bytetrain.prodinv.web.api.model.PriceAlteration
 import org.mapstruct.Mapper
 
 @Mapper(componentModel = "spring", uses = [PriceAlterationFactory::class, PriceModelMapper::class])
 interface PriceAlterationModelMapper :
-    EntityMapper<PriceAlteration, Price>
+    EntityMapper<PriceAlteration, com.bytetrain.prodinv.domain.PriceAlteration>
