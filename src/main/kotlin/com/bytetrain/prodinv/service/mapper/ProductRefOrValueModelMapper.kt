72645452999a98fce7d5564b995cf564cfb96a34
Frederika Frey <frfrey@ttu.ee>
2020-08-06 16:01:49 +0300

    Added ProductRefOrValueFactory and ProductRefOrValueModelMapperTest

--- src/main/kotlin/com/bytetrain/prodinv/service/mapper/ProductRefOrValueModelMapper.kt
+++ src/main/kotlin/com/bytetrain/prodinv/service/mapper/ProductRefOrValueModelMapper.kt
@@ -1,9 +1,10 @@
 package com.bytetrain.prodinv.service.mapper
 
 import com.bytetrain.prodinv.domain.ProductRefOrValueEntity
+import com.bytetrain.prodinv.service.mapper.factory.ProductRefOrValueFactory
 import com.bytetrain.prodinv.web.api.model.ProductRefOrValue
 import org.mapstruct.Mapper
 
-@Mapper(componentModel = "spring", uses = [CharacteristicModelMapper::class, ProductPriceModelMapper::class])
+@Mapper(componentModel = "spring", uses = [CharacteristicModelMapper::class, ProductPriceModelMapper::class, ProductRefOrValueFactory::class])
 interface ProductRefOrValueModelMapper :
     EntityMapper<ProductRefOrValue, ProductRefOrValueEntity>
