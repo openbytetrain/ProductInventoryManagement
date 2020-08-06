adac8782f910ceff6f40d7983c792fd656383f08
Egle Palk <eglepalk@gmail.com>
2020-08-06 11:15:13 +0300

    Fixed ProductAlterationModelMapperTest and fixed ProductAlterationFactory

--- src/main/kotlin/com/bytetrain/prodinv/service/mapper/factory/PriceAlterationFactory.kt
+++ src/main/kotlin/com/bytetrain/prodinv/service/mapper/factory/PriceAlterationFactory.kt
@@ -10,12 +10,38 @@ class PriceAlterationFactory(var priceModelMapper: PriceModelMapper) {
 
     @ObjectFactory
     fun priceAlterationToModelFactory(priceAlteration: PriceAlteration): com.bytetrain.prodinv.domain.PriceAlterationEntity {
-        return com.bytetrain.prodinv.domain.PriceAlterationEntity(priceAlteration.priceType, priceModelMapper.toEntity(priceAlteration.price))
+        return com.bytetrain.prodinv.domain.PriceAlterationEntity(
+            description = priceAlteration.description,
+            priceType = priceAlteration.priceType,
+            priceEntity = priceModelMapper.toEntity(priceAlteration.price),
+            applicationDuration = priceAlteration.applicationDuration,
+            name = priceAlteration.name,
+            priority = priceAlteration.priority,
+            recurringChargePeriod = priceAlteration.recurringChargePeriod,
+            unitOfMeasure = priceAlteration.unitOfMeasure,
+            productOfferingPrice = priceAlteration.productOfferingPrice,
+            atBaseType = priceAlteration.atBaseType,
+            atSchemaLocation = priceAlteration.atSchemaLocation,
+            atType = priceAlteration.atType
+        )
     }
 
     @ObjectFactory
     fun priceAlterationModelToPriceAlterationFactory(priceAlterationEntity: com.bytetrain.prodinv.domain.PriceAlterationEntity):
         PriceAlteration {
-        return PriceAlteration(priceAlterationEntity.priceType, priceModelMapper.toDto(priceAlterationEntity.priceEntity))
+        return PriceAlteration(
+            priceType = priceAlterationEntity.priceType,
+            price = priceModelMapper.toDto(priceAlterationEntity.priceEntity),
+            description = priceAlterationEntity.description,
+            applicationDuration = priceAlterationEntity.applicationDuration,
+            name = priceAlterationEntity.name,
+            priority = priceAlterationEntity.priority,
+            recurringChargePeriod = priceAlterationEntity.recurringChargePeriod,
+            unitOfMeasure = priceAlterationEntity.unitOfMeasure,
+            productOfferingPrice = priceAlterationEntity.productOfferingPrice,
+            atBaseType = priceAlterationEntity.atBaseType,
+            atSchemaLocation = priceAlterationEntity.atSchemaLocation,
+            atType = priceAlterationEntity.atType
+        )
     }
 }
--- src/test/kotlin/com/bytetrain/prodinv/service/mapper/PriceAlterationModelMapperTest.kt
+++ src/test/kotlin/com/bytetrain/prodinv/service/mapper/PriceAlterationModelMapperTest.kt
@@ -2,23 +2,13 @@ package com.bytetrain.prodinv.service.mapper
 
 import com.bytetrain.prodinv.domain.PriceAlterationEntity
 import com.bytetrain.prodinv.domain.PriceEntity
-import org.junit.Test
-import org.junit.jupiter.api.Assertions
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import java.net.URI
 
 @SpringBootTest
 class PriceAlterationModelMapperTest {
-
-    private var priceEntity = PriceEntity(
-        null,
-        null,
-        null,
-        null,
-        null,
-        null,
-        null)
 
     @Autowired
     private lateinit var priceAlterationModelMapper: PriceAlterationModelMapper
@@ -26,33 +16,29 @@ class PriceAlterationModelMapperTest {
     @Test
     fun `Test toDto with PriceAlterationModelMapper`() {
         var priceAlteration = priceAlterationModelMapper.toDto(generatePriceAlterationEntity())
-        org.assertj.core.api.Assertions.assertThat(priceAlteration.priceType == "priceType")
-        org.assertj.core.api.Assertions.assertThat(priceAlteration.priceEntity)
-        org.assertj.core.api.Assertions.assertThat(priceAlteration.applicationDuration == 0.5.toInt())
-        org.assertj.core.api.Assertions.assertThat(priceAlteration.description == "description")
-        org.assertj.core.api.Assertions.assertThat(priceAlteration.name == "name")
-        org.assertj.core.api.Assertions.assertThat(priceAlteration.priority == 0.6.toInt())
-        org.assertj.core.api.Assertions.assertThat(priceAlteration.recurringChargePeriod == "recurringChargePeriod")
-        org.assertj.core.api.Assertions.assertThat(priceAlteration.unitOfMeasure == "unitOfMeasure")
-        org.assertj.core.api.Assertions.assertThat(priceAlteration.productOfferingPrice == null)
-        org.assertj.core.api.Assertions.assertThat(priceAlteration.atBaseType == "atBaseType")
-        org.assertj.core.api.Assertions.assertThat(priceAlteration.atSchemaLocation == null)
-        org.assertj.core.api.Assertions.assertThat(priceAlteration.atType == "atType")
+        Assertions.assertThat(priceAlteration.priceType).isEqualTo("priceType")
+        Assertions.assertThat(priceAlteration.price.atBaseType).isEqualTo("String")
+        Assertions.assertThat(priceAlteration.applicationDuration).isEqualTo(1)
+        Assertions.assertThat(priceAlteration.description).isEqualTo("description")
+        Assertions.assertThat(priceAlteration.name).isEqualTo("name")
+        Assertions.assertThat(priceAlteration.priority).isEqualTo(6)
+        Assertions.assertThat(priceAlteration.recurringChargePeriod).isEqualTo("recurringChargePeriod")
+        Assertions.assertThat(priceAlteration.unitOfMeasure).isEqualTo("unitOfMeasure")
+        Assertions.assertThat(priceAlteration.atBaseType).isEqualTo("atBaseType")
+        Assertions.assertThat(priceAlteration.atType).isEqualTo("atType")
     }
 
     private fun generatePriceAlterationEntity(): PriceAlterationEntity {
         return PriceAlterationEntity(
             priceType = "priceType",
-            priceEntity = priceEntity,
-            applicationDuration = 0.5.toInt(),
+            priceEntity = PriceEntity(atBaseType = "String"),
+            applicationDuration = 1,
             description = "description",
             name = "name",
-            priority = 0.6.toInt(),
+            priority = 6,
             recurringChargePeriod = "recurringChargePeriod",
             unitOfMeasure = "unitOfMeasure",
-            productOfferingPrice = null,
             atBaseType = "atBaseType",
-            atSchemaLocation = null,
             atType = "atType"
         )
     }
